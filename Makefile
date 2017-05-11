f3031f80359ae91dbbe7f54bab7d643a48f2dd3f
Manuel Rodriguez <manuel.rodriguez@nxp.com>
2017-05-11 16:24:26 -0400

    Change Makefile and add stubs for bme280 module

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # If KERNELRELEASE is defined, we've been invoked from the
 # kernel build system and can use its language
 ifneq ($(KERNELRELEASE),)
-	obj-m := char_drv.o
+	obj-m := bme280.o
 
 # Otherwise we were called directly from the command
 # line; invoke the kernel build system.
--- bme280.c
+++ bme280.c
@@ -19,7 +19,46 @@
  * */
 
 #include <linux/module.h> 
+#include <linux/i2c.h>
 #include "bme280.h"
 
+static int bme280_probe(void){
+
+}
+
+static int bme280_remove(void){
+
+}
+
+static struct i2c_device_id bme280_idtable[] = {
+	{ "bme280", 0},
+	{ }
+};
+
+MODULE_DEVICE_TABLE(i2c, bme280_idtable);
+
+static struct i2c_driver bme280_driver = {
+	.driver = {
+		    .name = "bme280",
+		    .owner = THIS_MODULE,
+		  },
+	.probe = bme280_probe,
+	.remove = bme280_remove,
+	.id_table = bme280_idtable
+};
+
+
+static int __init bme280_init(void){
+	return 0;
+}
+
+static void __exit bme280_exit(void){
+
+}
+
+module_init(bme280_init);
+module_exit(bme280_exit);
+
 MODULE_AUTHOR("Manuel Rodriguez <manuel2982@gmail.com>");
+MODULE_DESCRIPTION("Driver for BME280 sensor");
 MODULE_LICENSE("GPL");
