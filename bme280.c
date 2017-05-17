d4257efeaf9ba2687e7f5fe3c25e79fbc3e8311a
Manuel Rodriguez <manuel.rodriguez@nxp.com>
2017-05-17 10:39:31 -0400

    Add stub functions for the driver

--- bme280.c
+++ bme280.c
@@ -1,6 +1,10 @@
 /*
  *  bme280.c - Linux device driver for the bme280 humidity,
  *  temperature and barometric sensor.
+ *
+ *  This driver uses the I2C bus subsystem and creates a char device when
+ *  probed to allow interaction with user space. For more details on use 
+ *  and implementation please refer to the README file.
  *  
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,24 +26,64 @@
 #include <linux/i2c.h>
 #include "bme280.h"
 
-static int bme280_probe(void){
+#define DEVICE_NAME "bme280"
 
+/* 
+ * Structure that holds bme280 calibration data - this data is loaded into
+ * the device at production and it is different for each device, this structure
+ * is populated in the probe function. 
+ * 
+ * */
+struct bme280_calibration{
+
+};
+
+/* Structure that holds bme280 configuration */
+struct bme280_configuration{
+
+};
+
+/* Structure that holds configuration and client data for the device */
+static struct bme280_data{
+	struct i2c_client *client;
+	struct bme280_calibration *cal_data;
+	struct bme280_configuration *cfg_data;
+};
+
+/*
+ * When the device is probed this function performs the following actions:
+ *  -Reads device ID - NOTE: I2C Address should be configured in the device tree (or the method
+ *                     used to register the device)
+ *  -Reads calibration parameters
+ *  -Configures device
+ *  -Registers char device for user space communication
+ * */
+static int __devinit bme280_probe(struct i2c_client *client, const struct i2c_device_id *id){
+	
+	/* Check if adapter supports the functionality we need */
+
+	/* Get chip_id */
+
+	/* Get calibration parameters */
+
+	/* Set configuration */
+
+	/* Register char device */
 }
 
-static int bme280_remove(void){
-
+static int __devexit bme280_remove(struct i2c_client *client){
+	/* Unregister char device */
 }
 
 static struct i2c_device_id bme280_idtable[] = {
-	{ "bme280", 0},
+	{DEVICE_NAME, 0},
 	{ }
 };
-
 MODULE_DEVICE_TABLE(i2c, bme280_idtable);
 
 static struct i2c_driver bme280_driver = {
 	.driver = {
-		    .name = "bme280",
+		    .name = DEVICE_NAME,
 		    .owner = THIS_MODULE,
 		  },
 	.probe = bme280_probe,
@@ -47,13 +91,23 @@ static struct i2c_driver bme280_driver = {
 	.id_table = bme280_idtable
 };
 
-
+/* Module init routine */
 static int __init bme280_init(void){
-	return 0;
+	/* Register driver within I2C subsystem */
+	int tmp;
+	tmp = i2c_add_driver(&bme280_driver);
+	if(tmp < 0){
+		printk(KERN_INFO "%s: I2C subsystem registration failed.\n", DEVICE_NAME);
+	}
+	printk(KERN_INFO "%s: Device successfully registered to I2C subsystem.\n", DEVICE_INFO);
+	return tmp;
 }
 
+/* Module exit routine */
 static void __exit bme280_exit(void){
 
+	/* Unregister driver from I2C subsystem */
+	i2c_del_driver(&bme280_driver);
 }
 
 module_init(bme280_init);
